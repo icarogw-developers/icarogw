d82a5dfbe858514bed1b6a3f7910fa1d5d52de8a
Simone Mastrogiovanni <mastrosi@roma1.infn.it>
2026-05-06 09:57:11 +0200

    last push

--- icarogw/likelihood.py
+++ icarogw/likelihood.py
@@ -5,6 +5,159 @@ import numpy as np
 import copy as cp
 from scipy.interpolate import RegularGridInterpolator
 
+
+class hierarchical_likelihood_v1_bright(bilby.Likelihood):
+    # Note the posterior_samples_dict_bright_list is a list correspnding
+    # to the various EM counterparts detected
+    def __init__(self, list_posterior_samples_dict_bright, list_detection_probabilities,
+                 injections, rate_model,
+                 nparallel_bright=None, neffPE=20, neffINJ=None, likelihood_variance_thr=None):
+
+        # Saves injections in a cupyfied format
+        self.injections=injections
+        self.neffPE=neffPE
+
+        # Rate models for bright and dark sirens
+        self.rate_model=rate_model
+
+        # Posterior samples for bright and dark sirens
+        self.list_posterior_samples_dict_bright=list_posterior_samples_dict_bright
+        
+        # Saves the detection probabilities models 
+        self.list_detection_probabilities=list_detection_probabilities
+      
+        self.list_distance_kdes_interpolant_bright = []
+
+        for i in range(len(self.list_posterior_samples_dict_bright)):
+            list_kde = []
+            self.list_posterior_samples_dict_bright[i].build_parallel_posterior(nparallel=nparallel_bright)        
+            for key in self.list_posterior_samples_dict_bright[i].posterior_samples_dict.keys():
+                self.list_posterior_samples_dict_bright[i].posterior_samples_dict[key].fit_gaussian_kde(['luminosity_distance','cos_theta_jn'])
+                
+                kde = self.list_posterior_samples_dict_bright[i].posterior_samples_dict[key].kde
+                
+                dl_interpo = np.linspace(self.list_posterior_samples_dict_bright[i].posterior_samples_dict[key].kde.dataset[0,:].min(), 
+                                         self.list_posterior_samples_dict_bright[i].posterior_samples_dict[key].kde.dataset[0,:].max(),
+                                         100)
+                
+                cos_theta_jn_interpo = np.linspace(self.list_posterior_samples_dict_bright[i].posterior_samples_dict[key].kde.dataset[1,:].min(),
+                                                 self.list_posterior_samples_dict_bright[i].posterior_samples_dict[key].kde.dataset[1,:].max(),
+                                                 200)
+                
+                XX, YY = np.meshgrid(dl_interpo, cos_theta_jn_interpo)
+                logpdfvals = kde.logpdf(np.vstack([XX.ravel(), YY.ravel()])).reshape(XX.shape)
+
+                ##################
+                #  Testing box
+                ##################
+                # import matplotlib.pyplot as plt
+                # plt.pcolormesh(XX, YY, np.exp(logpdfvals))
+                # plt.show(block=True)
+                # plt.close()
+                ##################
+                #  Testing box
+                ##################
+
+                list_kde.append(RegularGridInterpolator((dl_interpo, cos_theta_jn_interpo), logpdfvals.T,bounds_error=False, fill_value=-np.inf))
+                
+            # Each element of the lists below corresponds to the interpolant of the events for an EM counterpart
+            self.list_distance_kdes_interpolant_bright.append(cp.deepcopy(list_kde))
+
+      
+        self.likelihood_variance_thr = likelihood_variance_thr
+
+        if likelihood_variance_thr is not None:
+            print('Using Likelihood variance as numerical stability estimator \n We will not consider neffPE or neffINJ')
+            self.neffPE = -1.
+            self.neffINJ = -1.
+        else:    
+            print('Using neffPE and neffINJ as numerical stability estimators')
+            if neffINJ is None:
+                print('Setting neffINJ as 4 times observed signals')
+                nev_total = np.sum([self.list_posterior_samples_dict_bright[i].n_ev for i in range(len(self.list_posterior_samples_dict_bright))]) 
+                self.neffINJ=4*nev_total
+            else:
+                self.neffINJ=neffINJ
+        
+        super().__init__(parameters={ll: None for ll in self.rate_model.population_parameters})
+                
+    def log_likelihood(self):
+        '''
+        Evaluates and return the log-likelihood
+        '''          
+
+        self.rate_model.update(**{key:self.parameters[key] for key in self.rate_model.population_parameters})
+        self.injections.update_weights(self.rate_model)
+        Neff=self.injections.effective_injections_number()
+        # If the injections are not enough return 0, you cannot go to that point. This is done because the number of injections that you have
+        # are not enough to calculate the selection effect
+        
+        xp = get_module_array(self.injections.log_weights)
+        
+        if (Neff<self.neffINJ) | (Neff==0.):
+            return -xp.inf
+        
+        # Update the weights on the PE for BRIGHT sirens
+        self.rate_model.PEs_parameters = self.rate_model.PEs_parameters_bright # Needed to identify the relavant PEs parameters
+        self.rate_model.log_rate_PE = self.rate_model.log_rate_PE_bright
+        neff_PE_ev_bright = []
+        for i in range(len(self.list_posterior_samples_dict_bright)):
+            self.list_detection_probabilities[i].update_cosmology(self.rate_model.cw) # Update the cosmologies in the detection prob models
+            self.rate_model.pdet_EM = self.list_detection_probabilities[i] # Updates in the rate model the EM detection prob
+            self.rate_model.kde_dl_fits = self.list_distance_kdes_interpolant_bright[i] # Updates in the rate model the distance KDE for the bright sirens
+            self.list_posterior_samples_dict_bright[i].update_weights(self.rate_model)
+            neff_PE_ev_bright.append(self.list_posterior_samples_dict_bright[i].get_effective_number_of_PE())
+        neff_PE_ev_bright = xp.hstack(neff_PE_ev_bright)
+
+
+        neff_PE_ev_total = neff_PE_ev_bright
+
+        if xp.any(neff_PE_ev_total<self.neffPE):
+            return -xp.inf
+        
+        nev_total = xp.sum([self.list_posterior_samples_dict_bright[i].n_ev for i in range(len(self.list_posterior_samples_dict_bright))])
+        Ns_array = xp.hstack([self.list_posterior_samples_dict_bright[i].Ns_array for i in range(len(self.list_posterior_samples_dict_bright))])
+        self.likelihood_variance = (xp.power(nev_total,2.)/Neff)*(1-Neff/self.injections.ntotal)+xp.sum(
+            xp.power(neff_PE_ev_total,-1.)*(1-neff_PE_ev_total/Ns_array)
+        )
+
+        if self.likelihood_variance_thr is not None:
+            if self.likelihood_variance > self.likelihood_variance_thr:
+                return -xp.inf
+        
+        # Combine all the terms  
+        if self.rate_model.scale_free:
+            # Log likelihood for scale free model, Eq. 1.3 on the document
+            log_likeli = xp.sum([xp.sum(xp.log(self.list_posterior_samples_dict_bright[i].sum_weights))-
+                    self.list_posterior_samples_dict_bright[i].n_ev*xp.log(self.injections.pseudo_rate) 
+                    for i in range(len(self.list_posterior_samples_dict_bright))])
+            
+            # log_likeli = xp.sum([xp.sum(xp.log(self.list_posterior_samples_dict_bright[i].sum_weights))-
+            #         self.list_posterior_samples_dict_bright[i].n_ev*xp.log(self.rate_model.cw.cosmology.little_h**3.) 
+            #         for i in range(len(self.list_posterior_samples_dict_bright))])
+            
+        else:
+            Nexp=self.injections.expected_number_detections()
+            # Log likelihood for  the model, Eq. 1.1 on the document
+            log_likeli = -Nexp +xp.sum([self.list_posterior_samples_dict_bright[i].n_ev*xp.log(self.injections.Tobs)+
+                        xp.sum(xp.log(self.list_posterior_samples_dict_bright[i].sum_weights)) 
+                        for i in range(len(self.list_posterior_samples_dict_bright))]) 
+      
+        
+        # Controls on the value of the log-likelihood. If the log-likelihood is -inf, then set it to the smallest
+        # python valye 1e-309
+        if log_likeli == xp.inf:
+            raise ValueError('LOG-likelihood must be smaller than infinite')
+
+        if xp.isnan(log_likeli):
+            log_likeli = -xp.inf
+        else:
+            log_likeli = log_likeli
+            
+        return cp2np(log_likeli)
+
+
+
 class hierarchical_likelihood_v1_bright_and_dark(bilby.Likelihood):
     # Note the posterior_samples_dict_bright_list is a list correspnding
     # to the various EM counterparts detected
