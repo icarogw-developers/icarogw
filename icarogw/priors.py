07de467c52db9dab7c8fa9727d91fc4d19dd6428
Tom Bertheas <tom.bertheas@orange.fr>
2026-06-25 09:13:23 +0200

    Minors

--- icarogw/priors.py
+++ icarogw/priors.py
@@ -2490,8 +2490,7 @@ class logBspline_freeKnots_fromScipy(basic_1dimpdf):
         xn = get_module_array_scipy(x)
         t, coeffs = xp.asarray(self.t), xp.asarray(self.coeffs)
         interpolant = xn.interpolate.BSpline(t, coeffs, self.degree)
-        s_flat = interpolant(x.ravel())
-        return s_flat.reshape(x.shape)
+        return interpolant(x)
 
     def logZ(self):
         """
