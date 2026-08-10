4928b6f08f25f4532b618df2ac387d2cb2f9fd03
agnesepotenza99 <potenza.2192049@studenti.uniroma1.it>
2026-08-10 11:04:44 +0200

    Update wrappers.py

--- icarogw/wrappers.py
+++ icarogw/wrappers.py
@@ -3386,12 +3386,8 @@ class spinprior_Gaussian_to_Gaussian_windowGaussian():
     with w(m_1) = - sigma_t * sqrt(2pi) * (Guassian window function) + 1
     '''
     def __init__(self, mw):
-<<<<<<< HEAD
-        self.population_parameters= ['mu_chi_low_1', 'sigma_chi_low_1', 'mu_chi_high_1','sigma_chi_high_1', 'mu_t', 'sigma_t', 'mu_chi_2', 'sigma_chi_2', 'csi_spin']
-=======
         self.population_parameters= ['mu_chi_low_1', 'sigma_chi_low_1', 'mu_chi_high_1','sigma_chi_high_1', 'mu_t', 'sigma_t', 'mu_chi_2', 'sigma_chi_2','csi_spin']
->>>>>>> fd6a6b4b1c39abc05dfd040db7491d56ad73a962
-        self.event_parameters=['chi_1','chi_2','cos_t_1','cos_t_2', 'mass_1_source']
+        self.event_parameters=['chi_1','chi_2','cos_t_1','cos_t_2']
                
     def update(self,**kwargs):
         
@@ -3410,7 +3406,7 @@ class spinprior_Gaussian_to_Gaussian_windowGaussian():
         return (1 / (self.sigma_t * np.sqrt(2 * np.pi))) * np.exp(exp)
 
         
-    def log_pdf(self,chi_1,chi_2,cos_t_1,cos_t_2, mass_1_source,**kwargs):
+    def log_pdf(self,mass_1_source,**kwargs):
         
         xp = get_module_array(chi_1)
 
@@ -3428,9 +3424,9 @@ class spinprior_Gaussian_to_Gaussian_windowGaussian():
 
         return log_pdf_1 + log_pdf_2 + log_angular_part
         
-    def pdf(self,chi_1,chi_2,cos_t_1,cos_t_2,mass_1_source,**kwargs):
+    def pdf(self,mass_1_source,**kwargs):
         xp = get_module_array(mass_1_source)
-        return xp.exp(self.log_pdf(chi_1,chi_2,cos_t_1,cos_t_2,mass_1_source))
+        return xp.exp(self.log_pdf(mass_1_source,**kwargs))
 
 
 
