b1b3e72ecf7fbcec56f77b6945bb0b0202c43d5b
Simone Mastrogiovanni <mastrosi@roma1.infn.it>
2026-09-15 19:47:54 +0200

    Implemented GPU in DM

--- icarogw/density_contrast.py
+++ icarogw/density_contrast.py
@@ -1,5 +1,4 @@
-from scipy.interpolate import interpn
-import numpy as np
+from .cupy_pal import get_module_array, get_module_array_scipy
 
 class density_contrast_map(object):
     def __init__(self,dcenters,density_contrast_matrix,map):
@@ -8,15 +7,17 @@ class density_contrast_map(object):
         density_contrast: (realization,dcom,sky_index)
         map: mhealp map used for pixelization
         '''
-
+        
+        xp=get_module_array(dcenters)
         self.dcenters = dcenters
         self.density_contrast_matrix = density_contrast_matrix
         self.map = map
-        self.pixgrid = np.arange(self.map.npix)
+        self.pixgrid = xp.arange(self.map.npix)
 
     def get_density_contrast(self,dcom_h,skyindex):
-
-        return interpn((self.dcenters,self.pixgrid),
+        xp=get_module_array(dcom_h)
+        sx=get_module_array_scipy(dcom_h)
+        return sx.interpolate.interpn((self.dcenters,self.pixgrid),
                                       self.density_contrast_matrix,
-                                      np.column_stack([dcom_h,skyindex]), 
+                                      xp.column_stack([dcom_h,skyindex]), 
                                       bounds_error=False, fill_value=0.,method='splinef2d') # If a posterior sample falls outside, then you return 0
